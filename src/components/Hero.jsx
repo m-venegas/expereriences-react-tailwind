5166133a6384a17c8611550d54759af08c855721
Masiel <masiel31@msn.com>
2023-08-26 13:43:50 -0300

    Add Experiences

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div className='w-full h-screen'>
+     <img className='top-0 left-0 w-full h-screen object-cover ' src='https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg' alt="" />
+     <div className='bg-black/30 absolute top-0 left-0 h-screen w-full '/>
+     <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white '>
+        <div className=' md:left-[10%] max-w-[1100px] m-auto absolute p-4 '>
+        <p>All Inclusive</p>
+        <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl '>Private Beaches & Getaways</h1>
+        <p className='max-w[600px] drop-shadow-2xl py-2 text-xl '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, unde. Modi est nobis in a numquam. Iure ea culpa tempora.</p>
+        <button className='bg-white text-black '>Reserve Now</button>
+     </div>
+     </div>
+    </div>
+  )
+}
